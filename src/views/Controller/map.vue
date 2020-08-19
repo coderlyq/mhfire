2acac45fde821f4b2323a4497068021c537d0946
Levi <lyq_mailboxlist@163.com>
2020-08-19 17:41:34 +0800

    map topright div

--- src/views/Controller/map.vue
+++ src/views/Controller/map.vue
@@ -25,8 +25,8 @@ export default {
 					flag:true,
 					waterName:'水系统',
 					conts:{
-						"报警":2,
-						"故障":132
+						"报警":5,
+						"故障":152
 					}
 				}
 				},{
@@ -46,8 +46,8 @@ export default {
 						flag:true,
 						waterName:'水系统',
 						conts:{
-							"报警":2,
-							"故障":132
+							"报警":5,
+							"故障":152
 						}
 					}
 				}
@@ -84,7 +84,9 @@ export default {
 						enableMessage:true//设置允许信息窗发送短息
 						};
 			for(var i=0;i<data_info.length;i++){
-				var marker = new BMap.Marker(new BMap.Point(data_info[i][0],data_info[i][1]));  // 创建标注
+				//创建自定义图标
+				var myIcon = new BMap.Icon(require("../../assets/images/Controller/markFire.png"), new BMap.Size(31,47));
+				var marker = new BMap.Marker(new BMap.Point(data_info[i][0],data_info[i][1]),{icon:myIcon});  // 创建标注
 				var content = data_info[i][2];
 				map.addOverlay(marker);               // 将标注添加到地图中
 				addClickHandler(content,marker);
@@ -114,7 +116,7 @@ export default {
 			// 自定义控件必须实现自己的initialize方法,并且将控件的DOM元素返回
 			// 在本方法中创建个div元素作为控件的容器,并将其添加到地图容器中
 			ZoomControl.prototype.initialize = function(map){
-				// 创建一个DOM元素
+				// 创建一个DOM  topLeft元素
 				var div = document.createElement("div");
 				div.style.fontSize = '14px';
 				var inputSearch = document.createElement("input");
@@ -134,7 +136,8 @@ export default {
 				// nodeEle.innerText = lent[i].name;
 				for(var i = 0;i<lent.length;i++){
 					var nodeEles = document.createElement('div');
-					nodeEles.dataset.id = lent.id;
+					nodeEles.style.position = 'relative';
+					nodeEles.dataset.id = lent[i].id;
 					nodeEles.style.backgroundColor = "#fff";
 					nodeEles.style.boxSizing = "border-box"
 					nodeEles.style.width = "328px";
@@ -142,11 +145,13 @@ export default {
 					nodeEles.style.marginTop = "10px";
 					nodeEles.style.marginBottom = "10px";
 					nodeEles.style.paddingTop = "20px";
+					nodeEles.style.paddingBottom = "20px";
 					nodeEles.style.paddingLeft = "20px";
 					nodeEles.style.borderRadius = "10px";
 					// 列表标题
 					var nodeTitle = document.createElement("h2");
 					nodeTitle.style.fontFamily = "PFxi";
+					nodeTitle.style.margin = 0;
 					nodeTitle.style.color = "#000";
 					nodeTitle.style.fontSize = "14px";
 					// nodeEle.style.fontWeight = 'bolder';
@@ -154,29 +159,91 @@ export default {
 					//警情详情
 					if(lent[i].fire.flag){
 						var nodeDL = document.createElement('dl');
+						nodeDL.style.color = "#a1a1a1";
+						nodeDL.style.margin = 0;
+						nodeDL.style.marginTop = "16px";
 						var nodeDT = document.createElement('dt');
 						nodeDL.appendChild(nodeDT);
+						nodeDT.style.color = "#666";
+						nodeDT.style.fontFamily = "PFzc";
 						nodeDT.innerText = lent[i].fire.fireName;
+						nodeDT.style.marginBottom = "8px";
 						for (const key in lent[i].fire.conts) {
 							var nodeDD = document.createElement('dd');
 							nodeDD.style.display = "inline-block";
 							nodeDD.style.marginLeft = 0;
-							nodeDD.innerText = key+lent[i].fire.conts[key];
+							nodeDD.style.marginRight = "20px";
+							nodeDD.appendChild(document.createTextNode(key+":"));
+							var nodeDDSpanColor = '';
+							switch(key){
+								case '火警':
+									nodeDDSpanColor = '#fe3939';
+									break;
+								case '故障':
+									nodeDDSpanColor = '#fe8a27';
+									break;
+								case '启动':
+									nodeDDSpanColor = '#2dcdcf';
+									break;
+								case '反馈':
+									nodeDDSpanColor = '#29ca74';
+									break;
+							}
+							var nodeDDSpan = document.createElement('span');
+							nodeDDSpan.style.color = nodeDDSpanColor;
+							nodeDDSpan.innerText = lent[i].fire.conts[key];
+							nodeDD.appendChild(nodeDDSpan);
 							nodeDL.appendChild(nodeDD);
 						}
-						// for(var j = 0;j<lent[i].fire.conts.length;j++){
-						// 	var nodeDD = document.createElement('dd');
-						// 	nodeDD.innerText = "火警"+lent[i].fire.fireWarn;
-						// 	nodeDD.innerText = "故障"+lent[i].fire.fireTrouble;
-						// 	nodeDD.innerText = "启动"+lent[i].fire.fireStart;
-						// 	nodeDD.innerText = "反馈"+lent[i].fire.fireFeback;
-						// }
-						
 					}
+//水压水位详情
+					if(lent[i].water.flag){
+						var nodewaterDL = document.createElement('dl');
+						nodewaterDL.style.color = "#a1a1a1";
+						nodewaterDL.style.margin = 0;
+						nodewaterDL.style.marginTop = "16px";
+						var nodewaterDT = document.createElement('dt');
+						nodewaterDL.appendChild(nodewaterDT);
+						nodewaterDT.style.color = "#666";
+						nodewaterDT.style.fontFamily = "PFzc";
+						nodewaterDT.innerText = lent[i].water.waterName;
+						nodewaterDT.style.marginBottom = "8px";
+						for (const key in lent[i].water.conts) {
+							var nodewaterDD = document.createElement('dd');
+							nodewaterDD.style.display = "inline-block";
+							nodewaterDD.style.marginLeft = 0;
+							nodewaterDD.style.marginRight = "20px";
+							nodewaterDD.appendChild(document.createTextNode(key+":"));
+							var nodewaterDDSpanColor = '';
+							switch(key){
+								case '报警':
+									nodewaterDDSpanColor = '#fe3939';
+									break;
+								case '故障':
+									nodewaterDDSpanColor = '#fe8a27';
+									break;
+							}
+							var nodewaterDDSpan = document.createElement('span');
+							nodewaterDDSpan.style.color = nodewaterDDSpanColor;
+							nodewaterDDSpan.innerText = lent[i].water.conts[key];
+							nodewaterDD.appendChild(nodewaterDDSpan);
+							nodewaterDL.appendChild(nodewaterDD);
+						}
+					}
+					var mapSelectIcon = document.createElement('img');
+					mapSelectIcon.src = require("../../assets/images/Controller/mapSelectIcon.png");
+					mapSelectIcon.style.position = 'absolute';
+					mapSelectIcon.style.right = "20px";
+					mapSelectIcon.style.top = "12px";
 
 					div.appendChild(nodeEles);
 					nodeEles.appendChild(nodeTitle);
 					nodeEles.appendChild(nodeDL);
+					nodeEles.appendChild(nodewaterDL);
+					nodeEles.appendChild(mapSelectIcon);
+					nodeEles.addEventListener('click',function(e){
+						console.log(this.dataset.id);
+					});
 				}
 
 
@@ -189,14 +256,36 @@ export default {
 				div.style.height = "623px";
 				div.style.backgroundColor = "rgba(255,255,255,.5)";
 				div.style.borderRadius = "10px";
-				// 绑定事件,点击一次放大两级
-				div.onclick = function(e){
-				map.setZoom(map.getZoom() + 2);
-				}
+				map.enableScrollWheelZoom(true);
+				map.addEventListener("click",function(e){
+					// alert(e.point.lng + "," + e.point.lat);
+					map.setZoom(18);
+				});
+
+				//map topRight 
+				var topRightDiv = document.createElement('div');
+				topRightDiv.style.width = "366px";
+				topRightDiv.style.minHeight = "200px";
+				topRightDiv.style.backgroundColor = "rgba(255,255,255,.5)";
+				topRightDiv.style.position = 'absolute';
+				topRightDiv.style.right = "24px";
+				topRightDiv.style.top = "125px";
+
+				var topRightAlert = document.createElement('div');
+				topRightAlert.style.width = "358px";
+				topRightAlert.style.height = "196px";
+				topRightAlert.style.backgroundColor = "rgba(255,255,255,.5)";
+				topRightAlert.style.position = 'absolute';
+				topRightAlert.style.right = "405px";
+				topRightAlert.style.top = "168px";
+
 				// 添加DOM元素到地图中
 				map.getContainer().appendChild(div);
+				map.getContainer().appendChild(topRightDiv);
+				map.getContainer().appendChild(topRightAlert);
 				// 将DOM元素返回
 				return div;
+				//单击获取点击的经纬度
 			}
 			// 创建控件
 			var myZoomCtrl = new ZoomControl();
