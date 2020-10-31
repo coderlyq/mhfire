52e104ebe71a904a7a1a14eb9c891efc114dadfe
Levi <lyq_mailboxlist@163.com>
2020-10-31 17:24:50 +0800

    2020.10.31

--- src/views/Devide/DevideManage.vue
+++ src/views/Devide/DevideManage.vue
@@ -8,10 +8,21 @@
 					<el-input placeholder="通过设备名称/地址/IMEI查找设备" v-model="inputDevideCheck" suffix-icon="el-icon-search">
 						<!-- <el-button slot="append" icon="el-icon-search"></el-button> -->
 					</el-input>
-					<el-button type="primary">新增设备<i class="el-icon-circle-plus-outline el-icon--right"></i></el-button>
+					<el-button type="primary" @click="addTab(editableTabsValue)">新增设备<i slot="suffix" class="el-icon-circle-plus-outline el-icon--right"></i></el-button>
 				</div>
 			</el-header>
-			<el-main></el-main>
+			<el-main class="devideInfos">
+				<el-tabs v-model="editableTabsValue" type="card" closable @tab-remove="removeTab">
+					<el-tab-pane
+						v-for="item in editableTabs"
+						:key="item.name"
+						:label="item.title"
+						:name="item.name"
+					>
+						{{item.content}}
+					</el-tab-pane>
+				</el-tabs>
+			</el-main>
 		</el-container>
 	</div>
 </template>
@@ -24,10 +35,20 @@ export default {
 	name: "DevideManage",
 	//import引入的组件需要注入到对象中才能使用
 	data() {
-	//这里存放数据
-	return {
-
-	};
+		//这里存放数据
+		return {
+			editableTabsValue: '2',
+			editableTabs: [{
+				title: 'Tab 1',
+				name: '1',
+				content: 'Tab 1 content'
+			}, {
+				title: 'Tab 2',
+				name: '2',
+				content: 'Tab 2 content'
+			}],
+			tabIndex: 2
+		};
 	},
 	//监听属性 类似于data概念
 	computed: {},
@@ -35,7 +56,41 @@ export default {
 	watch: {},
 	//方法集合
 	methods: {
-
+		addTab(targetName) {
+			let newTabName = ++this.tabIndex + '';
+			this.editableTabs.push({
+				title: 'New Tab',
+				name: newTabName,
+				content: 'New Tab content'
+			});
+			this.editableTabsValue = newTabName;
+			console.log(targetName);
+		},
+		removeTab(targetName) {
+			console.log("123");
+						console.log(this.editableTabs);
+			console.log(this.editableTabsValue);
+						console.log("121344455567788");
+			let tabs = this.editableTabs;
+			let activeName = this.editableTabsValue;
+			console.log(activeName);
+			console.log(targetName);
+			if (activeName === targetName) {
+				tabs.forEach((tab, index) => {
+					if (tab.name === targetName) {
+						let nextTab = tabs[index + 1] || tabs[index - 1];
+						if (nextTab) {
+							activeName = nextTab.name;
+						}
+					}
+				});
+			}
+			this.editableTabsValue = activeName;
+			this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+		},
+		checkDevideList(){
+			console.log('checkDevideList');
+		}
 	},
 	//生命周期 - 创建完成（可以访问当前this实例）
 	created() {
@@ -55,5 +110,46 @@ export default {
 }
 </script>
 <style>
-
+	.devideManage{
+		background-color: #f2f4fa;
+		width: 100%;
+		height: 100%;
+	}
+	.devideManage .devideCont{
+		width: 100%;
+		height: 100%;
+	}
+	.devideManage .devideContTop{
+		height: 60px;
+		line-height: 60px;
+		background-color: #ffffff;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: stretch;
+	}
+	.devideManage .devideRight{
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+		align-items: center;
+	}
+	.devideManage .devideRight .el-input input{
+		width: 270px;
+		height: 30px;
+		padding-right: 20px;
+	}
+	.devideManage .devideRight .el-button{
+		width: 106px;
+		height: 30px;
+		line-height: 30px;
+		padding: 0;
+	}
+	.devideManage .devideInfos{
+		background-color: #ffffff;
+		width: 97%;
+		height: 97%;
+		margin: 20px;
+		border-radius: 10px;
+	}
 </style>
